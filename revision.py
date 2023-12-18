906fcaea55946cffdb85aec79639c472c8cfe34f
geeky-rahul <rahulx106@gmail.com>
2023-12-18 23:07:09 +0530

    jguu

--- revision.py
+++ revision.py
@@ -1,9 +1,9 @@
 a = 7
 
-# for i in range(a):
-#     for j in range(i):
-#         print("* ", end=" ")
-#     print(" ")
+for i in range(a):
+    for j in range(i):
+        print("* ", end=" ")
+    print(" ")
 
 # for i in range(a-1,0,-1):
 #     for j in range(i-1):
